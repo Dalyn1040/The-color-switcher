51a1a842450b8afe9c6a88ad96c6d7df3644bb38
Dalyn1040 <143821001+Dalyn1040@users.noreply.github.com>
2023-10-09 10:25:04 +0000

    intro

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let color = "yellow"
+function setbgcolor(){
+    document.getElementById("bg").style.backgroundColor = color;
+    document.getElementById("bgcolor").innerHTML = color;
+    document.getElementById("bgcolor").style.color = color;
+    
+
+}
+
+
